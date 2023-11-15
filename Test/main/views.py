d8d08e4a2b19d9dbfc1076b117fc3fca0b1775a9
chorn <chornyyaroslav5@gmail.com>
2023-11-15 11:34:59 +0200

    fix bugs

--- Test/main/views.py
+++ Test/main/views.py
@@ -64,10 +64,28 @@ class MyFormView(FormView):
         end_data = form.cleaned_data['end_data']
         
         if form.cleaned_data['use_template'] == True:
+            flipped_interval_mapping = {
+                '1 minute': 0.0166666667,
+                '3 minute': 0.05,
+                '5 minute': 0.0833333333,
+                '15 minute': 0.25,
+                '30 minute': 0.5,
+                '1 hour': 1,
+                '2 hour': 2,
+                '4 hour': 4,
+                '6 hour': 6,
+                '8 hour': 8,
+                '12 hour': 12,
+                '1 day': 24,
+                '3 day': 72,
+                '1 week': 168,
+                '1 month': 720
+            }
             template = get_object_or_404(Template, id=form.cleaned_data['selected_template'])
+            print(flipped_interval_mapping[template.interval],)
             form = MyForm(user=self.request.user.id, initial={
                 'symbol': template.symbol,
-                'interval': template.interval,
+                'interval': flipped_interval_mapping[template.interval],
                 'bound': template.bound,
                 'bound_unit': template.bound_unit,
                 'start_data': datetime.strptime(template.start_date, '%Y-%m-%d %H:%M:%S'),
@@ -91,7 +109,24 @@ class MyFormView(FormView):
                         return redirect('crypto')
                     else:
                         if form.cleaned_data['save_tamplates'] == True:
-                            Template.objects.create(user=self.request.user, name_exchange='Binance', name=f'Binance/{symbol}/{interval}/{start_data}/{end_data}/{bound}/{bound_unit}', symbol=symbol, interval=interval, bound=bound, bound_unit=bound_unit, start_date=start_data, end_date=end_data, min_interval=form.cleaned_data['custom_radio_field'])
+                            interval_mapping = {
+                                0.0166666667: '1 minute',
+                                0.05: '3 minute',
+                                0.0833333333: '5 minute',
+                                0.25: '15 minute',
+                                0.5: '30 minute',
+                                1.0: '1 hour',
+                                2.0: '2 hour',
+                                4.0: '4 hour',
+                                6.0: '6 hour',
+                                8.0: '8 hour',
+                                12.0: '12 hour',
+                                24.0: '1 day',
+                                72.0: '3 day',
+                                168.0: '1 week',
+                                720.0: '1 month'
+                            }
+                            Template.objects.create(user=self.request.user, name_exchange='Binance', name=f'Binance/{symbol}/{interval}/{start_data}/{end_data}/{bound}/{bound_unit}', symbol=symbol, interval=interval_mapping[float(interval)], bound=bound, bound_unit=bound_unit, start_date=start_data, end_date=end_data, min_interval=form.cleaned_data['custom_radio_field'])
                             messages.success(self.request, 'Шаблон был сохранен!')
                         task = Task.objects.create(user=self.request.user, is_running=True)
                         data = {
@@ -139,9 +174,22 @@ class SharesView(FormView):
         
         if form.cleaned_data['use_template'] == True:
             template = get_object_or_404(Template, id=form.cleaned_data['selected_template'])
+            interval_mapping = {
+                '1 minute': '1min',
+                '5 minute': '5min',
+                '15 minute': '15min',
+                '30 minute': '30min',
+                '45 minute': '45min',
+                '1 hour': '1h',
+                '2 hour': '2h',
+                '4 hour': '4h',
+                '1 day': '1day',
+                '1 week': '1week',
+                '1 month': '1month'
+            }
             form = SharesForm(user=self.request.user.id, initial={
                 'symbol': template.symbol,
-                'interval': template.interval,
+                'interval': interval_mapping[template.interval],
                 'bound': template.bound,
                 'bound_unit': template.bound_unit,
                 'start_data': datetime.strptime(template.start_date, '%Y-%m-%d %H:%M:%S'),
@@ -165,8 +213,21 @@ class SharesView(FormView):
                         return redirect('shares')
                     else:
                         if form.cleaned_data['save_tamplates'] == True:
-                                Template.objects.create(user=self.request.user, name_exchange='TwelveData', name=f'TwelveData/{symbol}/{interval}/{start_data}/{end_data}/{bound}/{bound_unit}', symbol=symbol, interval=interval, bound=bound, bound_unit=bound_unit, start_date=start_data, end_date=end_data, min_interval=form.cleaned_data['custom_radio_field'])
-                                messages.success(self.request, 'Шаблон был сохранен!')
+                            interval_mapping = {
+                                '1min': '1 minute',
+                                '5min': '5 minute',
+                                '15min': '15 minute',
+                                '30min': '30 minute',
+                                '45min': '45 minute',
+                                '1h': '1 hour',
+                                '2h': '2 hour',
+                                '4h': '4 hour',
+                                '1day': '1 day',
+                                '1week': '1 week',
+                                '1month': '1 month'
+                            }
+                            Template.objects.create(user=self.request.user, name_exchange='TwelveData', name=f'TwelveData/{symbol}/{interval}/{start_data}/{end_data}/{bound}/{bound_unit}', symbol=symbol, interval=interval_mapping[interval], bound=bound, bound_unit=bound_unit, start_date=start_data, end_date=end_data, min_interval=form.cleaned_data['custom_radio_field'])
+                            messages.success(self.request, 'Шаблон был сохранен!')
                         task = Task.objects.create(user=self.request.user, is_running=True)
                         data = {
                             'symbol': symbol,
@@ -616,6 +677,7 @@ def edit_template_polygon_view(request, profile_id):
         form = EditTemplatePolygonForm(request.POST)
 
         if form.is_valid():
+            exist = Template.objects.filter(user=request.user.id, name_exchange='Polygon', name=form.cleaned_data['name'] ).exists()
             symbol_validity = check_symbol_validity(form.cleaned_data['symbol'], form.cleaned_data['start_data'], form.cleaned_data['end_data'])
             if symbol_validity == "invalid symbol":
                 messages.error(request, 'Неверный символ!')
@@ -623,6 +685,22 @@ def edit_template_polygon_view(request, profile_id):
                 messages.error(request, 'Связка не может быть отрицательной!')
             elif form.cleaned_data['end_data'] < form.cleaned_data['start_data']:
                 messages.error(request, 'Дата окончания должна быть позже даты начала!')
+            elif exist:
+                if get_object_or_404(Template, id=profile_id).name != form.cleaned_data['name']:
+                    messages.error(request, 'Такое название шаблона уже существует!')
+                    return redirect('edit_template_polygon', profile_id=profile_id)
+                else:
+                    template.name = form.cleaned_data['name']
+                    template.choice = form.cleaned_data['choice']
+                    template.symbol = form.cleaned_data['symbol']
+                    template.interval = form.cleaned_data['interval']
+                    template.bound = form.cleaned_data['bound']
+                    template.bound_unit = form.cleaned_data['bound_unit']
+                    template.start_date = form.cleaned_data['start_data']
+                    template.end_date = form.cleaned_data['end_data']
+                    template.min_interval = form.cleaned_data['custom_radio_field']
+                    template.save()
+                    return redirect('template_polygon')
             else:
                 template.name = form.cleaned_data['name']
                 template.choice = form.cleaned_data['choice']
@@ -656,10 +734,23 @@ class EditTemplateTwelveDataView(View):
 
     def get(self, request, profile_id):
         template = get_object_or_404(Template, id=profile_id)
+        interval_mapping = {
+            '1 minute': '1min',
+            '5 minute': '5min',
+            '15 minute': '15min',
+            '30 minute': '30min',
+            '45 minute': '45min',
+            '1 hour': '1h',
+            '2 hour': '2h',
+            '4 hour': '4h',
+            '1 day': '1day',
+            '1 week': '1week',
+            '1 month': '1month'
+        }
         form = EditTemplateTwelveDataForm(initial={
             'name': template.name,
             'symbol': template.symbol,
-            'interval': template.interval,
+            'interval': interval_mapping[template.interval],
             'bound': template.bound,
             'bound_unit': template.bound_unit,
             'start_data': datetime.strptime(template.start_date, '%Y-%m-%d %H:%M:%S'),
@@ -672,20 +763,62 @@ class EditTemplateTwelveDataView(View):
         form = EditTemplateTwelveDataForm(request.POST)
 
         if form.is_valid():
+            exist = Template.objects.filter(user=self.request.user.id, name_exchange='TwelveData', name=form.cleaned_data['name'] ).exists()
             symbol_validity = check_symbol_validity(form.cleaned_data['symbol'], form.cleaned_data['start_data'], form.cleaned_data['end_data'])
             if symbol_validity == "invalid symbol":
                 messages.error(self.request, 'Неверный символ!')
-                return redirect('shares')
+                return redirect('edit_template_twelvedata', profile_id=profile_id)
             elif float(form.cleaned_data['bound']) < 0:
                 messages.error(self.request, 'Связка не может быть отрицательной!')
-                return redirect('shares')
+                return redirect('edit_template_twelvedata', profile_id=profile_id)
             elif form.cleaned_data['end_data'] < form.cleaned_data['start_data']:
                 messages.error(self.request, 'Дата окончания должна быть позже даты начала!')
-                return redirect('shares')
+                return redirect('edit_template_twelvedata', profile_id=profile_id)
+            elif exist:
+                if get_object_or_404(Template, id=profile_id).name != form.cleaned_data['name']:
+                    messages.error(self.request, 'Такое название шаблона уже существует!')
+                    return redirect('edit_template_twelvedata', profile_id=profile_id)
+                else:
+                    interval_mapping = {
+                        '1min': '1 minute',
+                        '5min': '5 minute',
+                        '15min': '15 minute',
+                        '30min': '30 minute',
+                        '45min': '45 minute',
+                        '1h': '1 hour',
+                        '2h': '2 hour',
+                        '4h': '4 hour',
+                        '1day': '1 day',
+                        '1week': '1 week',
+                        '1month': '1 month'
+                    }
+                    template.name = form.cleaned_data['name']
+                    template.symbol = form.cleaned_data['symbol']
+                    template.interval = interval_mapping[form.cleaned_data['interval']]
+                    template.bound = form.cleaned_data['bound']
+                    template.bound_unit = form.cleaned_data['bound_unit']
+                    template.start_date = form.cleaned_data['start_data']
+                    template.end_date = form.cleaned_data['end_data']
+                    template.min_interval = form.cleaned_data['custom_radio_field']
+                    template.save()
+                    return redirect('template_twelvedata')
             else:
+                interval_mapping = {
+                    '1min': '1 minute',
+                    '5min': '5 minute',
+                    '15min': '15 minute',
+                    '30min': '30 minute',
+                    '45min': '45 minute',
+                    '1h': '1 hour',
+                    '2h': '2 hour',
+                    '4h': '4 hour',
+                    '1day': '1 day',
+                    '1week': '1 week',
+                    '1month': '1 month'
+                }
                 template.name = form.cleaned_data['name']
                 template.symbol = form.cleaned_data['symbol']
-                template.interval = form.cleaned_data['interval']
+                template.interval = interval_mapping[form.cleaned_data['interval']]
                 template.bound = form.cleaned_data['bound']
                 template.bound_unit = form.cleaned_data['bound_unit']
                 template.start_date = form.cleaned_data['start_data']
@@ -714,10 +847,27 @@ class EditTemplateBinanceView(View):
 
     def get(self, request, profile_id):
         template = get_object_or_404(Template, id=profile_id)
+        flipped_interval_mapping = {
+                '1 minute': 0.0166666667,
+                '3 minute': 0.05,
+                '5 minute': 0.0833333333,
+                '15 minute': 0.25,
+                '30 minute': 0.5,
+                '1 hour': 1,
+                '2 hour': 2,
+                '4 hour': 4,
+                '6 hour': 6,
+                '8 hour': 8,
+                '12 hour': 12,
+                '1 day': 24,
+                '3 day': 72,
+                '1 week': 168,
+                '1 month': 720
+            }
         form = EditTemplateBinancesForm(initial={
             'name': template.name,
             'symbol': template.symbol,
-            'interval': template.interval,
+            'interval': flipped_interval_mapping[template.interval],
             'bound': template.bound,
             'bound_unit': template.bound_unit,
             'start_data': datetime.strptime(template.start_date, '%Y-%m-%d %H:%M:%S'),
@@ -730,19 +880,69 @@ class EditTemplateBinanceView(View):
         form = EditTemplateBinancesForm(request.POST)
 
         if form.is_valid():
+            exist = Template.objects.filter(user=self.request.user, name_exchange='Binance', name=form.cleaned_data['name'] ).exists()
             if form.cleaned_data['symbol'] not in get_binance_symbols():
                 messages.error(self.request, 'Invalid symbol!')
-                return redirect('crypto')
+                return redirect('edit_template_binance', profile_id=profile_id)
             elif float(form.cleaned_data['bound']) < 0:
                 messages.error(self.request, 'Связка не может быть отрицательной!')
-                return redirect('crypto')
+                return redirect('edit_template_binance', profile_id=profile_id)
             elif form.cleaned_data['end_data'] < form.cleaned_data['start_data']:
                 messages.error(self.request, 'Дата окончания должна быть позже даты начала!')
-                return redirect('crypto')
+                return redirect('edit_template_binance', profile_id=profile_id)
+            elif exist:
+                if get_object_or_404(Template, id=profile_id).name != form.cleaned_data['name']:
+                    messages.error(self.request, 'Такое название шаблона уже существует!')
+                    return redirect('edit_template_binance', profile_id=profile_id)
+                else:
+                    interval_mapping = {
+                        0.0166666667: '1 minute',
+                        0.05: '3 minute',
+                        0.0833333333: '5 minute',
+                        0.25: '15 minute',
+                        0.5: '30 minute',
+                        1.0: '1 hour',
+                        2.0: '2 hour',
+                        4.0: '4 hour',
+                        6.0: '6 hour',
+                        8.0: '8 hour',
+                        12.0: '12 hour',
+                        24.0: '1 day',
+                        72.0: '3 day',
+                        168.0: '1 week',
+                        720.0: '1 month'
+                    }
+                    template.name = form.cleaned_data['name']
+                    template.symbol = form.cleaned_data['symbol']
+                    template.interval = interval_mapping[float(form.cleaned_data['interval'])]
+                    template.bound = form.cleaned_data['bound']
+                    template.bound_unit = form.cleaned_data['bound_unit']
+                    template.start_date = form.cleaned_data['start_data']
+                    template.end_date = form.cleaned_data['end_data']
+                    template.min_interval = form.cleaned_data['custom_radio_field']
+                    template.save()
+                    return redirect('template_binance')
             else:
+                interval_mapping = {
+                    0.0166666667: '1 minute',
+                    0.05: '3 minute',
+                    0.0833333333: '5 minute',
+                    0.25: '15 minute',
+                    0.5: '30 minute',
+                    1.0: '1 hour',
+                    2.0: '2 hour',
+                    4.0: '4 hour',
+                    6.0: '6 hour',
+                    8.0: '8 hour',
+                    12.0: '12 hour',
+                    24.0: '1 day',
+                    72.0: '3 day',
+                    168.0: '1 week',
+                    720.0: '1 month'
+                }
                 template.name = form.cleaned_data['name']
                 template.symbol = form.cleaned_data['symbol']
-                template.interval = form.cleaned_data['interval']
+                template.interval = interval_mapping[float(form.cleaned_data['interval'])]
                 template.bound = form.cleaned_data['bound']
                 template.bound_unit = form.cleaned_data['bound_unit']
                 template.start_date = form.cleaned_data['start_data']
