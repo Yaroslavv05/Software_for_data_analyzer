3e43017cf244f7c4ac60fdfc319d3e321f85fe60
Yaroslavv05 <chornyyaroslav5@gmail.com>
2024-02-07 11:15:13 +0200

    fix bug

--- Test/main/tasks.py
+++ Test/main/tasks.py
@@ -1273,7 +1273,76 @@ def check_crossing_low(avg, previous_high, previous_low, date, symbol, timeframe
                 return output, status, crossed_avg 
     except Exception as e:
         print(e) 
-    return '1/0', 'ACTIVE', crossed_avg   
+    return '1/0', 'ACTIVE', crossed_avg
+
+
+def check_crossing_low_or_high(avg, previous_high, previous_low, date, symbol, timeframe):
+    try:
+        interval_mapping = {
+            '1 minute': 0.0166666667,
+            '5 minute': 0.0833333333,
+            '15 minute': 0.25,
+            '30 minute': 0.5,
+            '45 minute': 0.75,
+            '1 hour': 1.0,
+            '2 hour': 2.0,
+            '3 hour': 3.0,
+            '4 hour': 4.0,
+            '5 hour': 5.0,
+            '6 hour': 6.0,
+            '7 hour': 7.0,
+            '8 hour': 8.0,
+            '9 hour': 9.0,
+            '10 hour': 10.0,
+            '11 hour': 11.0,
+            '12 hour': 12.0,
+            '1 day': 24.0,
+            '1 week': 168.0,
+            '1 month': 720.0,
+            '1 year': 8760
+        }
+        
+        start_date = date
+        print(start_date)
+        start_date_datetime = datetime.strptime(start_date, '%Y-%m-%d %H:%M:%S')
+        if start_date_datetime.time() == datetime.strptime("00:00:00", "%H:%M:%S").time():
+            start_date_datetime = start_date_datetime.replace(hour=9, minute=30, second=0)
+        else:
+            start_date_datetime = datetime.strptime(start_date, '%Y-%m-%d %H:%M:%S')
+        ny_timezone = pytz.timezone('America/New_York')
+        start_date_datetime = ny_timezone.localize(start_date_datetime)
+        end_date_datetime = start_date_datetime + timedelta(hours=interval_mapping[timeframe])
+        start_unix_timestamp_milliseconds = int(start_date_datetime.timestamp()) * 1000
+        end_unix_timestamp_milliseconds = int(end_date_datetime.timestamp()) * 1000
+        url = f'https://api.polygon.io/v2/aggs/ticker/{symbol}/range/1/minute/{start_unix_timestamp_milliseconds}/{end_unix_timestamp_milliseconds}?adjusted=true&sort=asc&limit=50000&apiKey=EH2vpdYrp_dt3NHfcTjPhu0JOKKw0Lwz'
+        
+        response = requests.get(url).json()['results']
+        print(avg, previous_high, previous_low, start_unix_timestamp_milliseconds, end_unix_timestamp_milliseconds, symbol, timeframe)
+        crossed_avg = False
+
+        for i, candle in enumerate(response):
+            print(candle)
+            if crossed_avg == False and candle['o'] < avg and candle['h'] > avg:
+                crossed_avg = True
+                print('Было пересечение средины')
+            elif crossed_avg == False and candle['l'] < previous_low:
+                output = '2'
+                status = 'NOT ACTIVE'
+                return output, status 
+            elif crossed_avg == False and candle['h'] > previous_high:
+                output = '2'
+                status = 'NOT ACTIVE'
+                return output, status 
+            elif crossed_avg and candle['h'] > previous_high:
+                output = '1'
+                status = 'ACTIVE'
+                return output, status 
+            elif crossed_avg and candle['l'] < previous_low:
+                output = '0'
+                status = 'ACTIVE'
+                return output, status 
+    except Exception as e:
+        print(e)   
 
 
 def check_crossing_high(avg, previous_high, previous_low, date, symbol, timeframe):
@@ -1380,6 +1449,11 @@ def shares_polygon_new_async_task(data):
         trade = candle['n']
         amplitude = ((high - low) / low) * 100
         print(candle)
+        unix_timestamp_seconds = next_candle['t'] / 1000
+        unix_datetime = datetime.fromtimestamp(unix_timestamp_seconds, pytz.utc)
+        ny_timezone = pytz.timezone('America/New_York')
+        ny_datetime_next = unix_datetime.astimezone(ny_timezone)
+        next_time = ny_datetime_next.strftime("%Y-%m-%d %H:%M:%S")
         if interval_start <= amplitude <= interval_end:
             avg = (high + low) / 2
             next_high = next_candle['h']
@@ -1388,6 +1462,10 @@ def shares_polygon_new_async_task(data):
                 print(f'high - {high}\next_high - {next_high}\next_low - {next_low}\navg - {avg}\nlow - {low}')
                 status = 'NOT ACTIVE'
                 output = '2'
+                print(status, output)
+            elif next_high > high and next_low < low:
+                print(f'high - {high}\next_high - {next_high}\next_low - {next_low}\navg - {avg}\nlow - {low}')
+                output, status  = check_crossing_low_or_high(avg, high, low, next_time, symbol, timeframe)
                 print(status, output)
             elif next_high > avg and next_low < avg and next_high < high and next_low > low:
                 print(f'high - {high}\next_high - {next_high}\next_low - {next_low}\navg - {avg}\nlow - {low}')
@@ -1411,7 +1489,7 @@ def shares_polygon_new_async_task(data):
                 print(status, output)
             elif high < next_high and next_low < avg:
                 print(f'high - {high}\next_high - {next_high}\next_low - {next_low}\navg - {avg}\nlow - {low}')
-                output, status, crossed_avg  = check_crossing_high(avg, previous_high, previous_low, time, symbol, timeframe)
+                output, status, crossed_avg  = check_crossing_high(avg, high, low, next_time, symbol, timeframe)
                 print(status, output)
                 if output == '1/0' and status == 'ACTIVE' and crossed_avg == True:
                     previous_high = high
@@ -1429,7 +1507,7 @@ def shares_polygon_new_async_task(data):
                             break
             elif low > next_low and next_high > avg:
                 print(f'high - {high}\next_high - {next_high}\next_low - {next_low}\navg - {avg}\nlow - {low}')
-                output, status, crossed_avg = check_crossing_low(avg, previous_high, previous_low, time, symbol, timeframe)
+                output, status, crossed_avg = check_crossing_low(avg, high, low, next_time, symbol, timeframe)
                 print(status, output)
                 if output == '1/0' and status == 'ACTIVE' and crossed_avg == True:
                     previous_high = high
